2b6080595573a6ea9930b0d10827bc3da5d21815
Peyton Cross <peyton.cross@thoughtworks.com>
2021-06-16 13:06:33 -0700

    [1][Jeff & Peyton] Succesfully got script to stop running after failing test

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PRODUCER="${DIR}/../CitibikeApiProducer/"
 
 echo "====Building Producer JARs===="
-$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$PRODUCER/gradlew test -p $PRODUCER
+$PRODUCER/gradlew -p $PRODUCER clean bootJar
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt test && sbt package
-cd $DIR/../StationConsumer && sbt test && sbt package
+cd $DIR/../StationConsumer
+sbt test
+sbt package
+cd $DIR/../RawDataSaver
+sbt test
+sbt package
+
